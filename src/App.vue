a09acbe9a3d41845b9358bb75cf4ef30e9c1df3f
jennymae08 <jennymaedelatorre15@gmail.com>
2026-02-24 15:50:57 +0800

    announcement design

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,20 @@ import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 </script>
 
+<!-- max-w-7xl -->
+ 
 <template>
   <div class="min-h-screen w-full flex flex-col bg-slate-50 overflow-x-hidden">
     
     <Navbar />
 
-    <main class="flex-grow w-full">
-      <div class="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+    <main class="grow w-full">
+      <div class="mx-auto px-4 py-8 sm:px-6 lg:px-12"> 
         <RouterView />
       </div>
     </main>
 
-    <Footer />
+    <Footer/>
 
   </div>
 </template>
