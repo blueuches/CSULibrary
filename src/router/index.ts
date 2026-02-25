fb6b0e626799f1e3a6454a31a8066591a7f9c4fe
Dyanna Castro <dyannacastro04@gmail.com>
2026-02-25 15:13:48 +0800

    add other routes pages and update home/router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,9 +9,19 @@ import PersonnelPage from '../views/PersonnelPage.vue'
 import RecordPage from '../views/RecordPage.vue'
 import ServicesPage from '../views/ServicesPage.vue'
 
+//other route
+import Link1 from '@/views/OtherRoutes/Link1.vue'
+import Link2 from '@/views/OtherRoutes/Link2.vue'
+import Link3 from '@/views/OtherRoutes/Link3.vue'
+import BSPPage from '@/views/OtherRoutes/BSPPage.vue'
+import NBWCPage from '@/views/OtherRoutes/NBWCPage.vue'
+import Starbooks from '@/views/OtherRoutes/Starbooks.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.pBASE_URL),
   routes: [
+
+    //main navbars
     { path: '/', name: 'home', component: HomePage, },
     { path: '/about',name: 'about',component: AboutPage, },
     { path: '/announcements', name: 'announcements', component: AnnouncementPage,},
@@ -21,7 +31,13 @@ const router = createRouter({
     { path: '/records', name: 'records', component: RecordPage, },
     { path: '/services', name: 'services', component: ServicesPage, },
 
-    //
+    //other routes
+    { path: '/link1', name: 'link1', component: Link1, },
+    { path: '/link2', name: 'link2', component: Link2, },
+    { path: '/link3', name: 'link3', component: Link3, },
+    { path: '/bcp', name: 'bcppage', component: BSPPage, },
+    { path: '/nbwc', name: 'nbwcpage', component: NBWCPage, },
+    { path: '/starbooks', name: 'starbooks', component: Starbooks, },
   ],
 })
 
