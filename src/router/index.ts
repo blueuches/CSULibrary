4abb53d15fec482ef5f47228e4133f374f1ee6f5
PearlJanette <pearljanette.cacayan@carsu.edu.ph>
2026-03-04 08:17:10 +0800

    Deleting Feature in views, navbar, and in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import AboutPage from '../views/AboutPage.vue'
 import AnnouncementPage from '../views/AnnouncementPage.vue' 
-import FeaturesPage from '../views/FeaturesPage.vue'
 import GalleryPage from '../views/GalleryPage.vue'
 import PersonnelPage from '../views/PersonnelPage.vue'
 import RecordPage from '../views/RecordPage.vue'
@@ -29,7 +28,6 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomePage },
     { path: '/about', name: 'about', component: AboutPage },
     { path: '/announcements', name: 'announcements', component: AnnouncementPage },
-    { path: '/features', name: 'features', component: FeaturesPage },
     { path: '/gallery', name: 'gallery', component: GalleryPage },
     { path: '/personnel', name: 'personnel', component: PersonnelPage },
     { path: '/records', name: 'records', component: RecordPage },
