3efe2fc066a6671d65e01b52f04ad71b2031a2bf
bluechees <dengrcmrn@gmail.com>
2026-04-19 23:21:46 +0800

    RBAC Modal

--- src/components/ModalRBAC.vue
+++ src/components/ModalRBAC.vue
@@ -6,7 +6,7 @@ defineProps({
   }
 });
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(['close', 'confirm']);
 </script>
 
 <template>
@@ -42,7 +42,7 @@ const emit = defineEmits(['close']);
             <button
               type="button"
               class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              @click="emit('close')"
+              @click="emit('confirm')"
             >
               Understood
             </button>
